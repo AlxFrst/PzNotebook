f4412b4f75941adce01a9ce355365bb8652db99a
Alx <alexandreforestier1107@gmail.com>
2023-12-27 00:15:00 +0100

    Add initial project files and dependencies

---
+++ app.js
@@ -0,0 +1,106 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const sqlite3 = require('sqlite3').verbose();
+const path = require('path');
+
+// Initialiser Express
+const app = express();
+
+// Use public folder for static files
+app.use(express.static('public'));
+
+// Connexion à la base de données SQLite
+const db = new sqlite3.Database('pznotebook.db', (err) => {
+    if (err) {
+        console.error(err.message);
+    }
+    console.log('Connecté à la base de données SQLite.');
+});
+
+// Créer la table des catégories si elle n'existe pas
+const initDb = () => {
+    db.run(`CREATE TABLE IF NOT EXISTS categories (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        nom TEXT NOT NULL,
+        description TEXT,
+        objets TEXT
+    )`);
+};
+
+initDb();
+
+// Middleware pour parser le corps des requêtes
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+// Servir des fichiers statiques
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route pour la page d'accueil
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// Route pour ajouter une catégorie
+app.post('/add-category', (req, res) => {
+    const { nomCategorie, description, objets } = req.body;
+    const sql = `INSERT INTO categories (nom, description, objets) VALUES (?, ?, ?)`;
+    db.run(sql, [nomCategorie, description, JSON.stringify(objets)], (err) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ message: 'Catégorie ajoutée avec succès' });
+    });
+});
+
+// Route pour lister toutes les catégories
+app.get('/categories', (req, res) => {
+    const sql = `SELECT * FROM categories`;
+    db.all(sql, [], (err, rows) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ categories: rows });
+    });
+});
+
+app.post('/update-category', (req, res) => {
+    const { id, description, objets } = req.body;
+
+    // Préparation de la requête SQL pour la mise à jour
+    const sql = `UPDATE categories SET description = ?, objets = ? WHERE id = ?`;
+
+    // Exécution de la requête SQL avec les valeurs fournies
+    db.run(sql, [description, objets, id], function (err) {
+        if (err) {
+            // En cas d'erreur, envoyer une réponse d'erreur au client
+            res.status(400).json({ error: err.message });
+            return;
+        }
+
+        // Envoie d'une réponse de succès si la mise à jour a réussi
+        res.json({ message: `Catégorie avec l'ID ${id} mise à jour avec succès` });
+    });
+});
+
+app.delete('/delete-category/:id', (req, res) => {
+    const { id } = req.params;
+
+    const sql = 'DELETE FROM categories WHERE id = ?';
+    db.run(sql, id, function (err) {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({ message: `Catégorie avec l'ID ${id} supprimée avec succès` });
+    });
+});
+
+
+// Démarrer le serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Serveur démarré sur le port ${PORT}`);
+});
